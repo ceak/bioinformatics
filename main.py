7d92cead5f72cb1f570bf2f727692d216e8f67aa
Kirill Afanasiev <cyril1978@gmail.com>
2018-10-16 15:05:34 +0000

    ready set go

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-#!/usr/bin/python
-print ("Hello, World!")
+import sys
+
+print('Hello, World!')
+
+sum = int(sys.argv[1]) + int(sys.argv[2])
+
+print('The sum of {0} and {1} is {2}.'.format(sys.argv[1], sys.argv[2], sum))
